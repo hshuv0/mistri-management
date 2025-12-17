10010c8d02a3788a066dffd5d4ec48987b817436
hshuv0 <hshuvo571@gmail.com>
2025-12-17 13:10:27 +0600

    Update admin.py

--- mistri_info/admin.py
+++ mistri_info/admin.py
@@ -1,24 +1,16 @@
 from django.contrib import admin
 from .models import Mistri
 
-<<<<<<< HEAD
-# Register your models here.
-=======
 # # Register your models here.
 
-
-# @admin.register(Mistri)
-# class MistriAdmin(admin.ModelAdmin):
-#     list_display = ('image', 'name', 'skill', 'location', 'contact' )
->>>>>>> 6736dab (second commit)
 
 # admin.site.register(Mistri)
 
 @admin.register(Mistri)
 class MistriAdmin(admin.ModelAdmin):
-<<<<<<< HEAD
+
     list_display = ('image', 'name', 'skill', 'location', 'contact' )
-=======
+
     list_display = ('image', 'name', 'skill', 'location', 'contact')
     search_fields = ('name', 'skill', 'location')
->>>>>>> 6736dab (second commit)
+
