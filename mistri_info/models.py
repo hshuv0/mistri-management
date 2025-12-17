c26314c7e8cddc5e4f939d46e5dd6e6537ea73f9
hshuv0 <hshuvo571@gmail.com>
2025-12-17 13:00:43 +0600

    Update models.py

--- mistri_info/models.py
+++ mistri_info/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-<<<<<<< HEAD
 class Mistri(models.Model):
     image = models.ImageField(upload_to='mistri_info/', null=True, blank=True)
     name = models.CharField(max_length=100)
@@ -14,7 +13,7 @@ class Mistri(models.Model):
 
     def __str__(self):
         return self.name
-=======
+
 # class Mistri(models.Model):
 #     image = models.ImageField(upload_to='mistri_info/', null=True, blank=True)
 #     name = models.CharField(max_length=100)
@@ -38,5 +37,3 @@ class Mistri(models.Model):
 
     def __str__(self):
         return self.name
-    
->>>>>>> 6736dab (second commit)
