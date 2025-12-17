c8a8b1780179e95f079c2eff78b8596f8a54fd2c
hshuv0 <hshuvo571@gmail.com>
2025-12-17 13:07:15 +0600

    Update models.py

--- mistri_info/models.py
+++ mistri_info/models.py
@@ -13,27 +13,3 @@ class Mistri(models.Model):
 
     def __str__(self):
         return self.name
-
-# class Mistri(models.Model):
-#     image = models.ImageField(upload_to='mistri_info/', null=True, blank=True)
-#     name = models.CharField(max_length=100)
-#     # skill = models.CharField(max_length=50)
-#     location = models.CharField(max_length=100)
-#     contact = models.IntegerField()
-
-#     created_at = models.DateTimeField(auto_now_add = True)
-
-#     def __str__(self):
-#         return self.name
-
-class Mistri(models.Model):
-    image = models.ImageField(upload_to='mistri_info/', null=True, blank=True)
-    name = models.CharField(max_length=100)
-    skill = models.CharField(max_length=20, null=True)
-    location = models.CharField(max_length=20, null=True)
-    contact = models.IntegerField(null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
